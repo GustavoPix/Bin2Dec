cfa23757072efb0dc51570e83a6a669a96a726fd
Gustavo Carvalho <gustavo1net@hotmail.com>
2020-04-15 19:34:27 -0300

    melhora script, separador de bit e valor de bit

--- js/main.js
+++ js/main.js
@@ -2,17 +2,42 @@ const vm_main = new Vue({
     el:"#main",
     data:{
         input:"",
-        result:0
+        result:0,
+        bins:[]
     },
     methods: {
         
     },
     watch: {
         input(){
-            this.input = this.input.slice(0,8);
+            this.result = 0
+            this.bins = []
+            //this.input = this.input.slice(0,8);
+            this.input = this.input.replace(" ","");
             this.input = this.input.replace(/[^0-1]/g,"");
-            this.input = this.input.replace("-","");
-            this.result = this.input.length > 0 ? parseInt(this.input,2) : 0;
+            
+
+            let values = [];
+            values.push("");
+            for(let i = this.input.length - 1; i >= 0; i--)
+            {
+                if(values[values.length - 1].length == 8)
+                {
+                    values.push("");
+                }
+                values[values.length - 1] = this.input[i] + values[values.length - 1];
+            }
+            this.input = "";
+            values.reverse();
+            this.input = values.join(" ");
+
+            values.forEach(value => {
+                if(value)
+                {
+                    this.bins.push(parseInt(value,2));
+                    this.result += parseInt(value,2);
+                }
+            });
         }
     },
 });
